d663825ec5899bd8fa79f05087e6c2867336d6bc
Jaime <me@jescoto.com>
2021-06-04 15:11:31 -0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Program miprograma = new Program();
+
+        miprograma.name="Chrome";
+        miprograma.user="Developer-user";
+        miprograma.memorySize=1232.12;
+    }
+}
---
+++ src/com/company/Program.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Program {
+    String name;
+    String user;
+    double memorySize;
+
+    public Program()
+    {
+        name = "";
+        user = "";
+        memorySize=0.0;
+    }
+}
